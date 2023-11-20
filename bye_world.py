554b3331f300cf9b5795d121aeb3f0445dfad08a
unknown <fj22767@bristol.ac.uk>
2023-11-20 14:54:47 +0000

    good day

---
+++ bye_world.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Nov 20 14:52:32 2023
+
+@author: Dben
+"""
+
+print("bye_world")
--- hello_world.py
+++ hello_world.py
@@ -5,3 +5,4 @@ Created on Mon Nov 20 14:42:45 2023
 @author: Dben
 """
 
+print("hello world")
